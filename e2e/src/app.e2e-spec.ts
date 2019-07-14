9f5be7909c5a3975eed1b61e60e665a56fb03a3f
Leonardo Maglio <leotagit@gmail.com>
2019-07-14 19:52:49 +0200

    ng-p5 instances replaced by audioviz-experiments

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-p5!');
+    expect(page.getTitleText()).toEqual('Welcome to audioviz-experiments!');
   });
 
   afterEach(async () => {
